2584df47b3a4535ee4738a0721cb45cc3de86694
Brian Kohan <bckohan@gmail.com>
2023-01-15 03:09:58 -0800

    Add some basic pickling tests - close #24

--- enum_properties/tests/tests.py
+++ enum_properties/tests/tests.py
@@ -11,6 +11,8 @@ from enum_properties import (
     p,
     s,
 )
+from io import BytesIO
+import pickle
 
 
 class Unhashable:
@@ -1064,3 +1066,43 @@ class TestFlags(TestCase):
             [perm for perm in (Perm.R & Perm.X)],
             []
         )
+
+
+class TestPickle(TestCase):
+
+    def do_pickle_test(self, ipt):
+        buffer = BytesIO()
+        pickle.dump(ipt, buffer, pickle.HIGHEST_PROTOCOL)
+        buffer.seek(0)
+        opt = pickle.load(buffer)
+        return ipt is opt
+
+    def test_pickle(self):
+        from enum_properties.tests.pickle_enums import PriorityEx, Color
+        self.assertTrue(self.do_pickle_test(PriorityEx.ONE))
+        self.assertTrue(self.do_pickle_test(PriorityEx.TWO))
+        self.assertTrue(self.do_pickle_test(PriorityEx.THREE))
+
+        self.assertTrue(self.do_pickle_test(PriorityEx('1')))
+        self.assertTrue(self.do_pickle_test(PriorityEx('2')))
+        self.assertTrue(self.do_pickle_test(PriorityEx(3)))
+
+        self.assertTrue(self.do_pickle_test(Color.RED))
+        self.assertTrue(self.do_pickle_test(Color.GREEN))
+        self.assertTrue(self.do_pickle_test(Color.BLUE))
+
+    def test_flag_pickle(self):
+        from enum_properties.tests.pickle_enums import Perm, IntPerm
+        self.assertTrue(self.do_pickle_test(Perm.R))
+        self.assertTrue(self.do_pickle_test(Perm.W))
+        self.assertTrue(self.do_pickle_test(Perm.X))
+        self.assertTrue(self.do_pickle_test(Perm.RWX))
+        self.assertTrue(self.do_pickle_test(Perm.R | Perm.W | Perm.X))
+        self.assertTrue(self.do_pickle_test(Perm.R | Perm.W))
+
+        self.assertTrue(self.do_pickle_test(IntPerm.R))
+        self.assertTrue(self.do_pickle_test(IntPerm.W))
+        self.assertTrue(self.do_pickle_test(IntPerm.X))
+        self.assertTrue(self.do_pickle_test(IntPerm.RWX))
+        self.assertTrue(self.do_pickle_test(IntPerm.R | IntPerm.W | IntPerm.X))
+        self.assertTrue(self.do_pickle_test(IntPerm.W | IntPerm.X))
